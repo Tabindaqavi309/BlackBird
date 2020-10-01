e8a8719f27f8877218264354acf5bdd07b8e5ef9
Tabinda Qavi <tabindaqavi1@gmail.com>
2020-10-01 12:03:53 +0500

    third commit

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function BlackBird() {
   window.addEventListener("error", (error) => {
     console.log("*********************");
+    console.log("error is");
     console.log(error);
   });
-  console.log(window.navigator.appCodeName);
-  console.log(window.navigator.appName);
-  console.log(window.navigator.appVersion);
-  console.log(window.navigator.cookieEnabled);
-  console.log(window.navigator.language);
-  console.log(window.navigator.platform);
-  console.log(window.navigator.userAgent);
+  console.log("Browser CodeName:" + window.navigator.appCodeName);
+  console.log("Browser Name: " + window.navigator.appName);
+  console.log("Browser Version: " + window.navigator.appVersion);
+  console.log("Cookies Enabled: " + window.navigator.cookieEnabled);
+  console.log("Browser Language: " + window.navigator.language);
+  console.log("Platform:" + window.navigator.platform);
+  console.log("User-agent header:" + window.navigator.userAgent);
 }
 
 module.exports.BlackBird = BlackBird;
