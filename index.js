05670f4873a1a0f4b591a7d86a4efb8e8cb8ded0
Tabinda Qavi <tabindaqavi1@gmail.com>
2020-10-01 11:22:40 +0500

    new commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function BlackBird(data) {
-  console.log("Hello From BlackBird");
-  console.log(data);
+function BlackBird() {
+  window.addEventListener("error", (error) => {
+    console.log("*********************");
+    console.log(error);
+    //  console.log("this is error");
+  });
+  // function myFunction() {
+  //   if (window.top != window.self) {
+  //     document.getElementById("demo").innerHTML =
+  //       "This window is NOT the topmost window!";
+  //   } else {
+  //     document.getElementById("demo").innerHTML =
+  //       "This window is the topmost window!";
+  //   }
+  // }
+  // myFunction();
 }
 
 module.exports.BlackBird = BlackBird;
